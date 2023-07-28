04c1a118cb9ca5fbb5ba33797a2b19289ec62a2b
SabriImnadine <91572776+SabriImnadine@users.noreply.github.com>
2023-07-28 17:31:08 +0200

    Backend volledig

--- routes/loginsystem.js
+++ routes/loginsystem.js
@@ -4,94 +4,94 @@ const bcrypt = require('bcrypt');
 const {
   MongoClient
 } = require('mongodb');
-
+const {
+  v4: uuidv4
+} = require('uuid');
 const router = express.Router();
 require('dotenv').config();
- 
 const app = express();
-
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3500;
 
 app.use(cors());
 app.use(express.json());
 router.use(cors());
-router.use(express.json());
 
-// Config
-const Link = process.env.MONGODB_LINK;
-const client = new MongoClient(Link, {
+const link = process.env.MONGODB_LINK;
+const client = new MongoClient(link, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Connection
 client.connect((err) => {
   if (err) {
-    console.error('Error bij de connectie van de database :', err);
+    console.error('Fout bij het verbinden met de database:', err);
     return;
   }
-  console.log('Connected in MongoDB');
+  console.log('Verbonden met de MongoDB-database');
 });
 
-// Inschrijving
 app.post('/register', async (req, res) => {
   const {
     username,
     email,
     password
   } = req.body;
-  
+
   try {
-    // Check
-    const account = await client.db('Augustus-web2').collection('loginSysteem').findOne({
-      username
-    });
+    const account = await client
+      .db('Augustus-web2')
+      .collection('loginSysteem')
+      .findOne({
+        username
+      });
+
     if (account) {
       return res.status(400).json({
-        message: 'User al gebruikt'
+        message: 'Deze gebruikersnaam is al in gebruik'
       });
     }
 
-    // Check
-    const mail = await client.db('Augustus-web2').collection('loginSysteem').findOne({
-      email
-    });
-    if (mail) {
+    const actualEmail = await client
+      .db('Augustus-web2')
+      .collection('loginSysteem')
+      .findOne({
+        email
+      });
+
+    if (actualEmail) {
       return res.status(400).json({
-        message: 'Email al gebruikt'
+        message: 'Dit e-mail adres is al in gebruik'
       });
     }
 
-    // Check
     if (!username || !email || !password) {
       return res.status(400).json({
-        message: 'Elke veld moet ingevuld worden'
+        message: 'Vul a.u.b. alle verplichte velden in'
       });
     }
 
-    
     const cryptedPassword = await bcrypt.hash(password, 10);
+    const userId = uuidv4();
 
-    // New user in database
     await client.db('Augustus-web2').collection('loginSysteem').insertOne({
+      userId,
       username,
       email,
       password: cryptedPassword,
     });
 
     res.status(200).json({
-      message: 'Inschrijving voltooid'
+      message: 'succesvolle registratie',
+      userId
     });
   } catch (error) {
-    console.error('Error tijdens inscrijving :', error);
+    console.error('Fout bij het registreren:', error);
     res.status(500).json({
-      message: 'Een error tijdens inschrijving'
+      message: 'Er is een fout opgetreden tijdens het registreren'
     });
   }
 });
 
-// Connexion
 app.post('/login', async (req, res) => {
   const {
     email,
@@ -99,38 +99,164 @@ app.post('/login', async (req, res) => {
   } = req.body;
 
   try {
-    
-    const account = await client.db('Augustus-web2').collection('loginSysteem').findOne({
-      email
-    });
-    if (!account) {
+    const actualUser = await client
+      .db('Augustus-web2')
+      .collection('loginSysteem')
+      .findOne({
+        email
+      });
+
+    if (!actualUser) {
       return res.status(400).json({
-        message: 'Mail or password incorrect'
+        message: 'Onjuist e-mailadres of wachtwoord'
       });
     }
 
-    
-    const passwordMatch = await bcrypt.compare(password, existingUser.password);
-    if (!passwordMatch) {
+    const passwordSimlair = await bcrypt.compare(password, actualUser.password);
+    if (!passwordSimlair) {
       return res.status(400).json({
-        message: 'Mail or password incorrect'
+        message: 'Onjuist e-mailadres of wachtwoord'
       });
     }
 
     res.status(200).json({
-      message: 'Connexion succeed'
+      message: 'Succesvolle verbinding'
     });
   } catch (error) {
-    console.error('Error tijdens connectie :', error);
+    console.error('Fout tijdens het verbinden:', error);
     res.status(500).json({
-      message: 'Error tijdens connectie'
+      message: 'Er is een fout opgetreden bij het inloggen'
     });
   }
 });
 
-// Start
+app.post('/newChallenges', async (req, res) => {
+  const {
+    text,
+    description,
+    dataset,
+    picture,
+    result
+  } = req.body;
+  
+  const {
+    userId
+  } = req.body;
+  const challengeId = uuidv4();
+
+  try {
+    const actualUser = await client
+      .db('Augustus-web2')
+      .collection('loginSysteem')
+      .findOne({
+        userId
+      });
+
+    if (!actualUser) {
+      return res.status(400).json({
+        message: 'userID invalide'
+      });
+    }
+
+    await client.db('Augustus-web2').collection('challenges').insertOne({
+      challengeId,
+      userId,
+      text,
+      description,
+      dataset,
+      picture,
+      result,
+    });
+
+    res.status(200).json({
+      message: 'Uitdaging succesvol gemaakt',
+      challengeId,
+      userId
+    });
+    
+    
+  } catch (error) {
+    console.error('Fout bij het maken van de uitdaging:', error);
+    res.status(500).json({
+      message: 'Er is een fout opgetreden tijdens het maken van de uitdaging'
+    });
+  }
+});
+
+
+
+        
+app.delete('/deleteChallenge/:challengeId', async (req, res) => {
+  const { challengeId } = req.params;
+
+  try {
+    const deletedChallenge = await client
+      .db('Augustus-web2')
+      .collection('challenges')
+      .findOneAndDelete({ challengeId });
+
+    if (!deletedChallenge.value) {
+      return res.status(400).json({
+        message: 'Uitdaging niet gevonden of al verwijderd'
+      });
+    }
+
+    res.status(200).json({
+      message: 'Uitdaging succesvol verwijderd',
+      challengeId
+    });
+  } catch (error) {
+    console.error('Fout bij verwijderen van uitdaging:', error);
+    res.status(500).json({
+      message: 'Er is een fout opgetreden bij het verwijderen van de uitdaging'
+    });
+  }
+});
+
+
+
+
+app.get('/my-challenges', async (req, res) => {
+  const { userId } = req.query;
+
+  try {
+    const challenges = await client
+      .db('Augustus-web2')
+      .collection('challenges')
+      .find({ userId })
+      .toArray();
+
+    res.status(200).json({ challenges });
+  } catch (error) {
+    console.error('Fout bij ophalen van gebruikersuitdagingen:', error);
+    res.status(500).json({
+      message: 'Er is een fout opgetreden bij het ophalen van gebruikersuitdagingen'
+    });
+  }
+});
+
+app.get('/all-challenges', async (req, res) => {
+  try {
+    const challenges = await client
+      .db('Augustus-web2')
+      .collection('challenges')
+      .find({})
+      .toArray();
+
+    res.status(200).json({ challenges });
+  } catch (error) {
+    console.error('Fout bij het ophalen van alle uitdagingen:', error);
+    res.status(500).json({
+      message: 'Er is een fout opgetreden bij het ophalen van alle uitdagingen'
+    });
+  }
+});
+
+
+
+
 app.listen(port, () => {
-  console.log(`Server started in port ${port}`);
+  console.log(`Server is gestart op poort ${port}`);
 });
 
 module.exports = router;
